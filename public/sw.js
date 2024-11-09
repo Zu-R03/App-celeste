2973e11cd5742e416fbec8c5ae3fd82f993adda0
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-11-09 15:45:34 -0600

    fix

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('appShell').then(cache => {
             return cache.addAll([
-                '/images/IMG_9485.jpeg'
+                '/',                        // Asegura que la página principal esté cacheada
+                '/index.html',              // Reemplaza con la ruta del HTML si es necesario
+                '/images/IMG_9485.jpeg',    // Imagen en la carpeta public
+                '/images/IMG_9495.jpeg'
             ]);
         })
     );
