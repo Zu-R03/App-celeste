3d21d6d44be81087de471058785c2596e34f6cea
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-11-09 15:37:46 -0600

    fix

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('install', event => {
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('activate', event => {
-    caches.delete('appShell');
+    caches.delete('appShell2');
 });
 
 // eslint-disable-next-line no-restricted-globals
