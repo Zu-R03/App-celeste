4e22812c91da62853f7b93838124177780938bea
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-11-13 09:50:38 -0600

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Index from "./views";
+import Index from "./views/index";
 import Register from "./views/register";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/" element={<Index />} />
                 <Route path="/home" element={<Index />} />
-                <Route path="/sign-up" element={<Register />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
